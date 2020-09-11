d2e1b881f36484756b746bddb5c689d02a90e922
Joakim Frogner <joakim@inligo.no>
2020-09-11 14:02:17 +0200

    version

--- web/functions.php
+++ web/functions.php
@@ -4,4 +4,5 @@ Vesta::add_header_menu('Web Apps', '/plugin/vesta-inligo-apps/');
 
 if (Vesta::is_plugin_page('vesta-inligo-apps')) {
     Vesta::add_css('/plugin/vesta-inligo-apps/style.css');
+    echo "HELLO";
 }
