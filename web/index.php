60a138a9975ccf1547dbaecc8cd56519449fddc6
Joakim Frogner <joakim@inligo.no>
2020-09-15 12:30:45 +0200

    debug output

--- web/index.php
+++ web/index.php
@@ -1,5 +1,7 @@
 <?php
 
+$session_user = $_SESSION['user'];
+
 function save_server_name($server) {
     $server_name_path = '/usr/local/vesta/plugins/vesta-inligo-apps/plugin-data/server-name.txt';
     
@@ -68,20 +70,20 @@ if (isset($_POST['action']) && $_POST['action'] == "install"
 
     $output = '';
 
-    if ($user === $user_name) { 
+    if ($user === $user_name && $session_user === 'admin') { 
         if (is_numeric($port) && intval($port) >= 1000 && intval($port) <= 10000) {
             $port = intval($port);
             $conf_file = "/home/$user_name/web/$web_domain/private/app.nginx.conf";
             $conf = "proxy_pass http://127.0.0.1:$port;";
             $output = Vesta::exec("echo '$conf' 2>&1 > $conf_file");
             
-            if ($output) $output = "Error: Failed to write to $conf_file: $output";
+            if ($output) $output = "Error: Failed to write to $conf_file: $output. Current user: $user ($session_user)";
             else $output = "Successfully saved new config to $conf_file: $conf\nChange nginx proxy now under domain settings.";
         } else {
             $output = 'Port must be a numeric value between 1000 and 10000.';
         }
     } else {
-        $output = __("Only $user_name is allowed to perform this action.");
+        $output = __("Only $user_name authenticated through admin is allowed to perform this action.");
     }
     
     Vesta::render_cmd_output($output, __("Set application port"), $_SERVER['REQUEST_URI']);
